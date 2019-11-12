0c2208c9c05cb5e6d350b51b873b8c18ab00829e
DAOTIENNAM <daotiennamhl120699@gmail.com>
2019-11-12 20:43:58 +0700

    script to create table user and termNote

---
+++ create_tbl_user.sql
@@ -0,0 +1,24 @@
+USE [PRN292_ASS]
+GO
+
+/****** Object:  Table [dbo].[user]    Script Date: 11/12/2019 8:40:35 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[user](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [text] NULL,
+	[password] [text] NULL,
+	[email] [text] NULL,
+	[dob] [date] NULL,
+ CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
